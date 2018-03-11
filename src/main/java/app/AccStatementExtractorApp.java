456866f0b99d834fe3c04bd3fae230d2a9e2dd57
Hafiz Jaafar <m.hafiz.jaafar@outlook.com>
2018-03-11 17:23:41 +0100

    First commit.

---
+++ src/main/java/app/AccStatementExtractorApp.java
@@ -0,0 +1,12 @@
+package app;
+
+import org.apache.commons.cli.CommandLine;
+
+import java.text.ParseException;
+
+public class AccStatementExtractorApp {
+
+    public static void main(String[] argss) throws ParseException{
+        System.out.println("\n\tHello World! This is a dry run.\n");
+    }
+}
---
+++ src/main/java/imgProc/ImgProcessor.java
@@ -0,0 +1,4 @@
+package imgProc;
+
+public class ImgProcessor {
+}
